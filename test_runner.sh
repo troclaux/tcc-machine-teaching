7eff3f0f2a8d3c679c164432548c8ee9bd3bd086
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2022-09-19 17:09:08 -0300

    feat: saves solution id, saves import statement

--- test_runner.sh
+++ test_runner.sh
@@ -7,6 +7,11 @@ cd problems/736
 # run all files that start with solution_ and end with .py
 for file in solution_*.py; do
 	echo "Running $file"
+	# create variable str that saves the name of the file without the extension
+	str=${file%.*}
+	# remove the first 9 characters from the variable str
+	# solution_id=${str:9}
+	import_file="from ${str} import *"
 	#pytest test_736.py
 done
 
