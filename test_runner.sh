99207d3148cd54d77cdfd50f87d43287ea74b424
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2022-09-20 09:04:57 -0300

    feat: read problem_id from cmd line to run tests

--- test_runner.sh
+++ test_runner.sh
@@ -2,22 +2,27 @@
 # chmod +x test_runner.sh
 # ./test_runner.sh
 
-problem_id=736
+reset
 
-cd problems/$problem_id
+# for every problem id passed in the command line, run the tests
+for problem_id in "$@"
+do
+	cd problems/$problem_id
 
-# run all files that start with solution_ and end with .py
-for file in solution_*.py; do
-	echo "Running $file"
-	# create variable str that saves the name of the file without the extension
-	str=${file%.*}
-	# remove the first 9 characters from the variable str
-	import_file="from ${str} import *"
-	solution_id=${str:9}
-	echo "$import_file"
-	#replace first line of test_$problem_id.py with $import_file
-	sed -i "1s/.*/$import_file/" test_$problem_id.py
-	echo "Running test_$problem_id.py"
-	python3 -m pytest test_$problem_id.py
+	# run all files that start with solution_ and end with .py
+	for file in solution_*.py; do
+		echo "Running $file"
+		# create variable str that saves the name of the file without the extension
+		str=${file%.*}
+		# remove the first 9 characters from the variable str
+		import_file="from ${str} import *"
+		solution_id=${str:9}
+		echo "$import_file"
+		#replace first line of test_$problem_id.py with $import_file
+		sed -i "1s/.*/$import_file/" test_$problem_id.py
+		echo "Running test_$problem_id.py"
+		python3 -m pytest test_$problem_id.py
+	done
+
 done
 
