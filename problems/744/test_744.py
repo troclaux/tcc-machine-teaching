8f59036e514a3aacc9dc254258f5f286f9907469
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2022-09-21 19:48:50 -0300

    test: add final test cases to cover all partitions

--- problems/744/test_744.py
+++ problems/744/test_744.py
@@ -1,13 +1,15 @@
-from solution_26744 import *
+from solution_26712 import *
 
 import pytest
 
 test_cases = [
 	('', '###'),
+	('a', '##a#'),
 	('ab','#a#b#'),
-	('abc','#a#bc#'),
+	('abc','#a#bc#')
 ]
 
 @pytest.mark.parametrize("s, output", test_cases)
+
 def test_hashtag(s, output):
 	assert hashtag(s) == output
