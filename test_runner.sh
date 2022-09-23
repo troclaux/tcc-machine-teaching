3159081c70de7a4b0d6e2a0767dfbc0cd3291908
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2022-09-22 22:03:21 -0300

    refactor: removes .txt creation when saving output

--- test_runner.sh
+++ test_runner.sh
@@ -22,13 +22,12 @@ do
 		sed -i "1s/.*/$import_file/" test_$problem_id.py
 		echo "Running test_$problem_id.py"
 		# write pytest output in a file
-		python3 -m pytest test_$problem_id.py> output.txt
-		# save pytest output to a variable
-		output=$(cat output.txt)
+		# python3 -m pytest test_$problem_id.py> output.txt
+		output=`python3 -m pytest test_$problem_id.py`
 		# save the word after test_$problem_id.py in the variable output
 		output=${output#*test_$problem_id.py }
 		echo "Output: $output"
+		
 	done
-
 done
 
