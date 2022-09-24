b02abfb3174c339b91401da654ca3e9b11d353b1
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2022-09-24 18:44:06 -0300

    refactor: make test runner only runs tests

--- test_runner.sh
+++ test_runner.sh
@@ -8,8 +8,6 @@ reset
 for problem_id in "$@"; do
 
 	cd problems/$problem_id
-	count_solutions = 0
-	> output.txt
 
 	# run all files that start with solution_ and end with .py
 	for file in solution_*.py; do
@@ -25,29 +23,9 @@ for problem_id in "$@"; do
 		echo "Running test_$problem_id.py"
 		# write pytest output in a file
 		# python3 -m pytest test_$problem_id.py> output.txt
-		output=`python3 -m pytest test_$problem_id.py`
-		# save the word after test_$problem_id.py in the variable output
-		output=${output#*test_$problem_id.py }
-		# save the first word in ouput in the variable result
-		result=${output%% *}
-		echo "result: $result"
-		# echo "output: $output"
-		count_perfect_solutions=0
-		# if result contains only . or F characters, then the test passed
-		if [[ $result =~ ^[.F]+$ ]]; then
-			echo "valid pytest output for solution $solution_id"
-			echo "$result" >> output.txt
-			# if result contains only . characters, then increment count_perfect_solutions
-			if [[ $result =~ ^[.]+$ ]]; then
-				count_perfect_solutions=$((count_perfect_solutions+1))
-			fi
-		else
-			echo "invalid pytest output for solution $solution_id"
-			echo "ERROR" >> output.txt
-		fi
-		# invalid output in solution_50936.py
-		echo "total number of solutions: $count_solutions"
-		echo "perfect solutions: $count_perfect_solutions"
+		python3 -m pytest test_$problem_id.py
+
 	done
+
 done
 
