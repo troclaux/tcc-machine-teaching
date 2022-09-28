3aa965c32ac30104de7798fba27f441ca4b5c8e1
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2022-09-28 14:51:55 -0300

    chore: remove creation of folder 'solutions'

--- depickle_problems.py
+++ depickle_problems.py
@@ -15,17 +15,10 @@ with open('problems.pkl', 'rb') as f:
         path = os.path.join(path, name_id)
         path_txt = os.path.join(path, name_id_txt)
         path_df = os.path.join(path, name_id_csv)
-        #path_tests = os.path.join(path, "tests/")
-        path_solutions = os.path.join(path, "solutions/")
-
         os.makedirs(os.path.dirname(path_txt), exist_ok=True)
         DataFrame = pd.DataFrame(columns=["ID", "Total_tests", "Passed_tests", "Original_outcome"])
         DataFrame.to_csv(path_or_buf = path_df, index=False)
 
-        #if not os.path.exists(path_tests):
-        #    os.mkdir(os.path.dirname(path_tests))
-        if not os.path.exists(path_solutions):
-            os.mkdir(os.path.dirname(path_solutions))
         with open(path_txt, 'a', encoding="utf-8") as g:
             g.write("chapter__label:")
             g.write('\n')
