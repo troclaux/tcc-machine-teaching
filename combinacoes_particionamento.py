0491279ebde39b8827beaeadbf5c0e2b25c73dca
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2022-09-30 14:29:16 -0300

    feat: get all combinations coverage

--- combinacoes_particionamento.py
+++ combinacoes_particionamento.py
@@ -1,4 +1,5 @@
-from itertools import combinations
+from calendar import c
+from itertools import combinations, product
 import pyperclip
 
 # Particionamento para rp = 3, rs = 3, rpq = 3, rsq = 3
@@ -14,7 +15,7 @@ partitions = [element.upper() for element in partitions]
 def pretty_print_matrix(matrix):
   print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in matrix]))
 
-def get_requirements(partition_list):
+def get_pair_wise_coverage(partition_list):
   combs = list(combinations(partition_list, 2))
 
   for combination in list(combs):
@@ -23,9 +24,17 @@ def get_requirements(partition_list):
 
   return list(combs)
 
+# ACoC
+acoc_partitions = [["a", "b"], ["1", "2", "3"], ["x", "y"]]
 
-combinations = get_requirements(partitions)
+def get_all_combinations_coverage(partition_list):
+  # make combination for a list with any number of partitions
+  combs = list(product(*partition_list))
+  print(str(combs))
 
+get_all_combinations_coverage(acoc_partitions)
+
+combinations = get_pair_wise_coverage(partitions)
 
 def remove_quot(myStr):
   outputString = ''
