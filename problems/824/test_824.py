0ba5b93c45675a3c427efc7529c4d024684064ce
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2022-10-03 11:12:44 -0300

    test: add test cases for all combination strategies

--- problems/824/test_824.py
+++ problems/824/test_824.py
@@ -2,12 +2,44 @@ from solution_121511 import *
 
 import pytest
 
-test_cases = [
-	("hello world", "HeLLo WoRLD"),
-
+test_cases_all_combinations = [
+	("ok", "oK"),
+	("bom", "BoM"),
+	("a", "a"),
+	("boa", "Boa"),
+	("tarde", "TaRDe"),
+	("ae", "ae"),
+	("b", "B"),
+	("kd", "KD"),
+	("a x", "a X"),
+	("e kd", "e KD"),
+	("e no", "e No"),
+	("bom dia", "BoM Dia"),
+	("ae ae", "ae ae"),
+	("b x", "B X"),
+	("kd x", "KD X"),
 ]
 
-@pytest.mark.parametrize("frase, output", test_cases)
+test_cases_each_choice = [
+	("e", "e"),
+	("x", "X"),
+	("hello world", "HeLLo WoRLD"),
+]
+
+test_cases_pair_wise = [
+	("ok","oK"),
+	("nunca", "NuNCa"),
+	("a", "a"),
+	("b", "B"),
+	("a b", "a B"),
+	("a e", "a e"),
+	("b c", "B C"),
+	("e kd", "e KD"),
+	("pai", "Pai"),
+	("kd", "KD")
+]
+
+@pytest.mark.parametrize("frase, output", test_cases_pair_wise)
 
 def test_uppCons(frase, output):
 	assert uppCons(frase) == output
