9922ef134da06bc9f6208f8ef4f5941f8e546cca
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2022-10-03 21:04:53 -0300

    test: add test cases for problem 806

--- problems/806/test_806.py
+++ problems/806/test_806.py
@@ -1,10 +1,12 @@
-from solution_50910 import *
+from solution_37112 import *
 
 import pytest
 
 test_cases = [
 	((0, 0, 1, 1), (0, 0, 1, 1), True),
-
+	((-2, -2, -1, -1), (0, 0, 1, 1), False),
+	((0, 0, 1, 1), (-2, -2, -1, -1), False),
+	((-2, -2, -1, -1), (-2, -2, -1, -1), True),
 ]
 
 @pytest.mark.parametrize("tupla1, tupla2, output", test_cases)
