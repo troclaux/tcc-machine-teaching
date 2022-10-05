9f9d6b3e2174404e74efe34478b5d25dcf5d8af2
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2022-10-05 16:51:11 -0300

    feat: overwrite first line of test

--- test_runner_input.py
+++ test_runner_input.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import subprocess
 
 for problem_id in sys.argv[1:]:
 
@@ -13,9 +14,7 @@ for problem_id in sys.argv[1:]:
 		if 'solution_' not in solution_filename:
 			continue
 
-
 		print(os.getcwd())
-
 		print(solution_filename)
 
 		buffer = solution_filename.split('_')
@@ -23,7 +22,18 @@ for problem_id in sys.argv[1:]:
 		solution_id = buffer[0]
 
 		test_filename = "test_input_" + problem_id + ".py"
-		import_str = "from solution_" + solution_id + " import *"
+		import_str = "from solution_" + solution_id + " import *\n"
+
+		with open(test_filename, 'r', encoding='utf-8') as file:
+			data = file.readlines()
+
+		print(data)
+		data[0] = import_str
+
+		with open(test_filename, 'w', encoding='utf-8') as file:
+			file.writelines(data)
+
+		# os.system("pytest " + test_filename)
 
 	os.chdir('..')
 
