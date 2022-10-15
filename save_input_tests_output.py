dcac09a31c556f526339928e3de7a5acddd5999e
François Boéchat <fran_5298@hotmail.com>
2022-10-15 16:51:05 -0300

    Added timeout

--- save_input_tests_output.py
+++ save_input_tests_output.py
@@ -12,29 +12,29 @@ for problem_id in sys.argv[1:]:
 
 	for solution_filename in pwd_list:
 
-		if 'solution_' not in solution_filename:
+		if 'solution_' not in solution_filename:	
 			continue
 
 		solution_id = solution_filename[9:-3]
 
 		import_str = " import_stmts.solution_" + solution_id
 		test_str = "test_input_" + problem_id + ".py"
-		cmd = f"pytest {test_str} --tb=line --solution {import_str}"
+		cmd = f"pytest {test_str} --tb=line --solution {import_str} --timeout=2"
 		# os.system(cmd + " >> output_input.txt")
 
 		try:
 			output_str = subprocess.check_output(cmd, shell=True).decode('utf-8')
-			result = output_str.split("\n")[6]
+			result = output_str.split("\n")[9]
 			result = result.split(" ")[1]
 
 		except Exception as e:
-			# print(f"ERROR {solution_id}:", e.output.decode('utf-8'))
+			#print(f"ERROR {solution_id}:", e.output.decode('utf-8'))
 
 			e_str = str(e)[-2]
 
 			if e_str == '1':
 				output_str = e.output.decode('utf-8')
-				result = output_str.split("\n")[6]
+				result = output_str.split("\n")[9]
 				result = result.split(" ")[1]
 			elif e_str == '2':
 				result = "ERROR"
