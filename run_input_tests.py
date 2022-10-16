80dd184db77ebf3ef56ae710938e8268076df038
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2022-10-16 08:11:09 -0300

    chore: update comments and command to run tests

--- run_input_tests.py
+++ run_input_tests.py
@@ -8,6 +8,8 @@ for problem_id in sys.argv[1:]:
 
 	pwd_list = sorted(os.listdir())
 
+	test_str = "test_input_" + problem_id + ".py"
+
 	for solution_filename in pwd_list:
 
 		if 'solution_' not in solution_filename:
@@ -16,11 +18,13 @@ for problem_id in sys.argv[1:]:
 		print(os.getcwd())
 		print(solution_filename)
 
+		#extract solution id
 		solution_id = solution_filename[9:-3]
-
 		import_str = " import_stmts.solution_" + solution_id
-
-		os.system("pytest --tb=line --solution " + import_str)
+		# define the command to run
+		cmd = f"pytest {test_str} --tb=line --solution {import_str} --timeout=2"
+		# run test command
+		os.system(cmd)
 
 	os.chdir('..')
 
