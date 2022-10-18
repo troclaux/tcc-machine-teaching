d3ea5f4fe658ee77c44dff850a93951ab3dcfef9
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2022-10-18 15:44:41 -0300

    test: add all test cases for problem 821

---
+++ problems/821/conftest.py
@@ -0,0 +1,10 @@
+#from email.policy import default
+import pytest
+
+def pytest_addoption(parser):
+        parser.addoption("--solution", action="store", default="accident")
+
+def pytest_generate_tests(metafunc):
+    option_value = metafunc.config.option.solution
+    if 'solution' in metafunc.fixturenames and option_value is not None:
+        metafunc.parametrize("solution", [option_value])
