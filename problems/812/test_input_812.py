0dcbc1a21fe4e84f82f08a62ba0d93f9f4fece21
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2022-10-18 09:06:06 -0300

    test: add all test cases for problem 812

---
+++ problems/812/test_input_812.py
@@ -0,0 +1,13 @@
+import pytest
+import importlib
+
+test_cases = [
+	('Olá, hoje cheguei no trabalho e perguntei ao meu chefe: - Preciso voltar mais cedo; já que preciso sair 16 horas', 'Olá  hoje cheguei no trabalho e perguntei ao meu chefe   Preciso voltar mais cedo  já que preciso sair 16 horas'),
+	('Boa noite.', 'Boa noite '),
+]
+
+@pytest.mark.parametrize("str, output", test_cases)
+
+def test_retira_pontuacao(str, output, solution):
+	imp = importlib.import_module(solution)
+	assert imp.retira_pontuacao(str) == output
