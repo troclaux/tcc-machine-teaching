db6980a83bf1af3cf2faf7aae5382730957e5f90
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2022-10-19 17:46:02 -0300

    fix: add commas between test_cases

--- problems/819/test_input_819.py
+++ problems/819/test_input_819.py
@@ -2,10 +2,9 @@ import pytest
 import importlib
 
 test_cases = [
-
-	([1, 2], 2, [2])
-	([-1.1, -3.3, -2.2], -1.1, [-1.1, -3.3, -2.2])
-	([3, 5], 4, [])
+	([1, 2], 2, [2]),
+	([-1.1, -3.3, -2.2], -1.1, [-1.1, -3.3, -2.2]),
+	([3, 5], 4, []),
 ]
 
 @pytest.mark.parametrize("lista_numero, n, output", test_cases)
