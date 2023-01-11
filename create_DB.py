ec09f30bbb174cf8b0f56c6fd3b3a8991d7f52dc
François Boéchat <fran_5298@hotmail.com>
2023-01-11 14:18:22 -0300

    Feat: Create DB and Insert atributes

--- create_DB.py
+++ create_DB.py
@@ -1,24 +1,4 @@
-'''import sys
-import os
 import mysql.connector
-
-mydb = mysql.connector.connect(
-  host="127.0.0.1:3306",
-  user="root",
-  password="RootSenha"
-)
-
-mycursor = mydb.cursor()
-
-mycursor.execute("CREATE DATABASE tcc_machine_teaching")
-mycursor.execute("SHOW DATABASES")
-
-for db in mycursor:
-  print(db)
-'''
-import mysql.connector
-
-
 
 # Connect to the MySQL server
 #cnx = mysql.connector.connect(user='root', password='RootSenha', host='127.0.0.1', database='tcc_machine_teaching')
@@ -36,6 +16,9 @@ except:
 # Connect to the MySQL Database
 cursor.execute("USE tcc_machine_teaching")
 
+cursor.execute("DROP TABLE solution")
+cursor.execute("DROP TABLE test")
+
 # Create the "Test" table
 try:
   create_table_query = "CREATE TABLE Test ( test_id INTEGER PRIMARY KEY AUTO_INCREMENT, problem_id INTEGER, criteria_type VARCHAR(255), pass_count INTEGER, fail_count INTEGER, error_count INTEGER)"
@@ -46,12 +29,11 @@ except:
 # Create the "Solution" table
 try:
   create_table_query = "CREATE TABLE Solution (solution_id INTEGER PRIMARY KEY AUTO_INCREMENT, problem_id INTEGER, old_outcome BOOLEAN, new_outcome BOOLEAN, result VARCHAR(255))"
+  cursor.execute(create_table_query)
 except:
   print("Solution exists")
 
 # FOREIGN KEY (test_id) REFERENCES Test(id)
-
-cursor.execute(create_table_query)
 
 # Commit the changes to the database
 cnx.commit()
--- depickle_solutions.py
+++ depickle_solutions.py
@@ -1,14 +1,31 @@
 import pickle
 from tqdm import tqdm
 import os
+import mysql.connector
+# Connect to the MySQL server
+#cnx = mysql.connector.connect(user='root', password='RootSenha', host='127.0.0.1', database='tcc_machine_teaching')
+cnx = mysql.connector.connect(user='root', password='RootSenha', host='127.0.0.1')
+cnx.autocommit = True
+
+# Create a cursor to execute SQL statements
+cursor = cnx.cursor(buffered=True)
+
+# Try create database
+try:
+  cursor.execute("CREATE DATABASE tcc_machine_teaching")
+except:
+  print("db exists")
+
+# Connect to the MySQL Database
+cursor.execute("USE tcc_machine_teaching")
+
 
 def create_import_stmts():
     path = os.path.join(directory, "problems")
-    #os.system(f"cd {path}")
     for problem in sorted(os.listdir(path)):
         path_problem = os.path.join(path, problem)
         path_stmts = os.path.join(path_problem, "import_stmts.py")
-        os.system(f"> {path_stmts}")
+        open(f"{path_stmts}", "w")
 
 with open('solutions.pkl', 'rb') as f:
     data = pickle.load(f)
@@ -16,16 +33,20 @@ with open('solutions.pkl', 'rb') as f:
 
     create_import_stmts()
 
+    cursor.execute("TRUNCATE TABLE solution")
+    cursor.execute("TRUNCATE TABLE test")
     for i, enunciado in enumerate(tqdm(data)):
         problem_id = str(enunciado["problem_id"])
         solution_id = "solution_" + str(i)
+        if enunciado["outcome"] == "P":
+            solution_outcome = 1
+        else:
+            solution_outcome = 0
         solution_id_path = "solution_" + str(i) + ".py"
-        # solution_txt = str(i) + ".txt"
         path = os.path.join(directory, "problems")
         path = os.path.join(path, problem_id)
         path_id = os.path.join(path, solution_id_path)
         path_stmts = os.path.join(path, "import_stmts.py")
-        # path_txt = os.path.join(path, solution_txt)
 
         if not os.path.exists(path):
             os.makedirs(path)
@@ -36,6 +57,12 @@ with open('solutions.pkl', 'rb') as f:
 
         with open(path_stmts, 'a', encoding="utf-8") as h:
             h.write(f"try:\n\timport {solution_id} \nexcept:\n\tprint('{solution_id} IMPORT ERROR')\n")
+
+
+
+        insert_solution = f"INSERT INTO solution (problem_id, old_outcome) VALUES({problem_id} ,{solution_outcome})"
+        cursor.execute(insert_solution)
+        cursor.execute("SELECT * from solution")
 
 
 
