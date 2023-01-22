23aa19470f2c10d721c3597dbc308244c357de40
François Boéchat <fran_5298@hotmail.com>
2023-01-22 15:34:36 -0300

    Add: reference text and source folder

--- depickle_problems.py
+++ depickle_problems.py
@@ -1,6 +1,7 @@
 import pickle
 import os
 import pandas as pd
+import shutil
 
 with open('problems.pkl', 'rb') as f:
     data = pickle.load(f)
@@ -11,18 +12,30 @@ with open('problems.pkl', 'rb') as f:
         name_id = str(enunciado["id"])
         name_id_txt = name_id + ".txt"
         name_id_csv = name_id + ".csv"
+
+        path_src = os.path.join(directory, "tests")
+        path_src_conf = os.path.join(path_src, "conftest.py")
+        path_src = os.path.join(path_src, name_id)
+        path_src_input = os.path.join(path_src, f"test_input_{name_id}")
+        path_src_graph = os.path.join(path_src, f"test_graph_{name_id}")
+        path_src_mutation = os.path.join(path_src, f"test_mutation_{name_id}")
+        
         path = os.path.join(directory, "problems")
         path = os.path.join(path, name_id)
+
         path_txt = os.path.join(path, name_id_txt)
         path_df = os.path.join(path, name_id_csv)
+        path_conf = os.path.join(path, "conftest.py")
+        path_input = os.path.join(path, f"test_input_{name_id}")
+        path_graph = os.path.join(path, f"test_graph_{name_id}")
+        path_mutation = os.path.join(path, f"test_mutation_{name_id}")
+
         os.makedirs(os.path.dirname(path_txt), exist_ok=True)
-        DataFrame = pd.DataFrame(
-            columns=["id",
-            "passed_criteria_tests",
-            "passed_graph_tests",
-            "passed_mutation_tests",
-            "original_outcome"])
-        DataFrame.to_csv(path_or_buf = path_df, index=False)
+
+        shutil.copy(path_src_conf, path_conf)
+        shutil.copy(path_src_input, path_input)
+        shutil.copy(path_src_graph, path_graph)
+        shutil.copy(path_src_mutation, path_mutation)
 
         with open(path_txt, 'a', encoding="utf-8") as g:
             g.write("chapter__label:")
---
+++ tests/736/test_input_736.py
@@ -0,0 +1,20 @@
+#import import_stmts
+import sys
+import pytest
+import importlib
+
+test_cases = [
+	('1', '', '11'),
+	('', '1', '11'),
+	('', '', ''),
+	('x', 'y', 'xyyx'),
+	('1', '2', '1221'),
+]
+@pytest.fixture
+def solution():
+	return solution
+
+@pytest.mark.parametrize("a, b, output", test_cases)
+def test_concatenacao(a, b, output, solution):
+	imp = importlib.import_module(solution)
+	assert imp.concatenacao(a, b) == output
