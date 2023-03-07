b2c071bd24646e82c61b2aa262bffe575a124fd2
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2023-03-06 21:06:02 -0300

    feat: update test cases for problem 811

--- tests/811/test_input_811.py
+++ tests/811/test_input_811.py
@@ -2,12 +2,15 @@ import pytest
 import importlib
 
 test_cases = [
-	([1, 2, 3], 2, 3, True),
-	([1, 2, 3], 3, 2, True),
-	([2, 1, 3], 2, 3, True),
-	([2, 1, 3], 3, 2, True),
-	([3, 1, 2], 2, 3, True),
-	([3, 1, 2], 3, 2, True),
+	([10, 20, 30], 50, 50, True),
+	([1, 10, 20], 5, 5, False),
+	([10, 20, 30], 5, 5, False),
+	([1, 10, 15], 5, 20, True),
+	([1, 10, 15], 20, 5, True),
+	([10, 10, 30], 20, 5, False),
+	([10, 10, 30], 5, 20, False),
+	([10, 30, 40], 20, 5, False),
+	([10, 30, 40], 5, 20, False),
 ]
 
 @pytest.mark.parametrize("lst, h, w, output", test_cases)
