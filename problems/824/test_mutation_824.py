e69c541e0de862442e69d4812d83b979bca6bda0
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2023-03-10 15:08:09 -0300

    chore: remove deprecated test files

--- problems/824/test_mutation_824.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase, main
-from lab7 import uppCons
-
-class TestaUppCons(TestCase):
-    def test_simples(self):
-        self.assertEqual(uppCons('abcdefghi'),'aBCDeFGHi')
-
-if __name__ == '__main__':
-    main()
