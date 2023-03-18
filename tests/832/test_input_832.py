75561718c9e515778b910176a4848d78f200a77a
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2023-03-18 09:11:56 -0300

    feat: add input test cases for ex 832

---
+++ tests/832/test_input_832.py
@@ -0,0 +1,17 @@
+import pytest
+import importlib
+
+test_cases = [
+	([[1]], True),
+	([], True),
+	([[1],[2]], False),
+	([[30, 40]], False),
+    ([[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]], True),
+]
+
+@pytest.mark.parametrize("matriz, output", test_cases)
+
+def test_lingua_p(matriz, output, solution):
+	imp = importlib.import_module(solution)
+	assert imp.lingua_p(matriz) == output
+
