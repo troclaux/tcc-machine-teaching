1fc93ec85d6cdb3d02f1550f9230931591ea60c3
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2023-03-18 13:20:00 -0300

    feat: add input test cases for ex 834

---
+++ tests/834/test_input_834.py
@@ -0,0 +1,20 @@
+import pytest
+import importlib
+
+test_cases = [
+	([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 5.0),
+	([[-5]], -5.0),
+	([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]], 8.5)
+	([[1]], 1.0),
+	([[-1, -2], [-3, -4], [-5,-6]], -3.5),
+	([[1, 2, 0], [0, 5, 6], [7, 0, 9], [10, 11, 0]], 4.25)
+	([[-1, 2, 3, -4], [5, -6, -7, 8], [9, -10, -11, -12]], -2.0)
+	([[30, 95]], 62.5),
+]
+
+@pytest.mark.parametrize("matriz, output", test_cases)
+
+def test_lingua_p(matriz, output, solution):
+	imp = importlib.import_module(solution)
+	assert imp.lingua_p(matriz) == output
+
