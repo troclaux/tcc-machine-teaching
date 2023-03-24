6da79febd985146acdde6c11d4a45d38d02d9cc4
François Boéchat <fran_5298@hotmail.com>
2023-03-24 09:19:23 -0300

    fix test_input_800

--- tests/800/test_input_800.py
+++ tests/800/test_input_800.py
@@ -2,17 +2,17 @@ import pytest
 import importlib
 
 produtos = {
-    'amaciante':4.99,
-    'arroz':10.90,
-    'biscoito':1.69,
-    'cafe':6.98,
-    'chocolate':3.79,
-    'farinha':2.99
+    "amaciante":4.99,
+    "arroz":10.90,
+    "biscoito":1.69,
+    "cafe":6.98,
+    "chocolate":3.79,
+    "farinha":2.99
 }
 
 
 test_cases = [
-	(["amaciante", "arroz", 'arroz', "biscoito", "cafe", 'biscoito' ,'arroz',"chocolate"], produtos, 51.84)
+	(["amaciante", "arroz", "arroz", "biscoito", "cafe", "biscoito" ,"arroz","chocolate"], produtos, 51.84),
 	(["farinha"], produtos, 2.99)
 ]
 
